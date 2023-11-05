a36242562974affc898a52803c78587b04058948
Adam Rosas <adamrosas27@gmail.com>
2023-11-05 08:56:54 -0800

    Add constructor method and add boilerplate for knapsack_solver and calculate_max_value methods

---
+++ knapsackSolver.py
@@ -0,0 +1,15 @@
+# Solving the 0/1 Knapsack Problem with Dynamic Programming using the Tabulation Method
+
+
+class Knapsack:
+
+    def __init__(self, weights, values, capacity):
+        self.weights = weights
+        self.values = values
+        self.capactity = capacity
+
+    def knapsack_solver(self, weights, values, capacity):
+        pass
+
+    def calculate_max_value(self):
+        pass
