d08b56217b04ed369bce150269082151593158c3
Adam Rosas <adamrosas27@gmail.com>
2023-11-05 12:40:01 -0800

    Iterate through indicies in the table to fill with values/weights

--- knapsackSolver.py
+++ knapsackSolver.py
@@ -7,9 +7,13 @@ class Knapsack:
         self.items = items
         self.capacity = capacity
 
-    def knapsack_solver(weights, values, capacity):
+    def knapsack_solver(items, capacity):
         # Create 2 dimensional grid/table to hold values
         table = [[0] * (capacity + 1) for _ in range(len(items) + 1)]
+
+        for row in range(1, len(items) + 1):
+            for column in range(1, capacity + 1):
+                item_weight, item_value = items[i - 1]
 
     def calculate_max_value(self):
         pass
