72cc95fb3002405ca3e8586cb84cc2fd0db3b939
Adam Rosas <adamrosas27@gmail.com>
2023-11-05 09:07:54 -0800

    Refactor constructor method and initialized table/grid in knapsack_solver method

--- knapsackSolver.py
+++ knapsackSolver.py
@@ -3,13 +3,13 @@
 
 class Knapsack:
 
-    def __init__(self, weights, values, capacity):
-        self.weights = weights
-        self.values = values
-        self.capactity = capacity
+    def __init__(self, items, capacity):
+        self.items = items
+        self.capacity = capacity
 
-    def knapsack_solver(self, weights, values, capacity):
-        pass
+    def knapsack_solver(weights, values, capacity):
+        # Create 2 dimensional grid/table to hold values
+        table = [[0] * (capacity + 1) for _ in range(len(items) + 1)]
 
     def calculate_max_value(self):
         pass
